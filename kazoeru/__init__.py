9c2ac20f8b030d052527adb9936ecb9f0618162d
Keksi <contact@keksi.me>
2023-09-21 19:01:12 +0200

    chore: fix docker

--- kazoeru/__init__.py
+++ kazoeru/__init__.py
@@ -1,5 +1,7 @@
 import logging
 import sys
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
 
 import dotenv
 from rich.logging import RichHandler
@@ -12,9 +14,20 @@ dotenv.load_dotenv()
 
 
 def setup_logging():
-    logging.basicConfig(level=logging.INFO, handlers=[RichHandler(rich_tracebacks=True)])
+    format_string = "%(asctime)s | %(name)s | %(levelname)s | %(message)s"
+    log_format = logging.Formatter(format_string)
 
-    logging.getLogger("disnake").setLevel(logging.WARNING)
+    log_file = Path("logs/kazoeru.log")
+    log_file.parent.mkdir(exist_ok=True)
+
+    file_handler = RotatingFileHandler(log_file, maxBytes=5 * (2**20), backupCount=10, encoding="utf-8")
+    file_handler.setFormatter(log_format)
+
+    rich_handler = RichHandler(rich_tracebacks=True)
+
+    logging.basicConfig(level=logging.DEBUG, handlers=[file_handler, rich_handler])
+
+    # logging.getLogger("disnake").setLevel(logging.WARNING)
 
 
 setup_logging()
